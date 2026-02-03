afdf84655182ac5e2041262370a631f18383e18f
qweru <119245015+gkursi@users.noreply.github.com>
2026-02-03 18:21:11 +0200

    feat: Vec3d

---
+++ multirender-api/src/main/kotlin/multirender/api/math/Vec3d.kt
@@ -0,0 +1,14 @@
+package xyz.qweru.multirender.api.math
+
+interface Vec3d {
+    companion object {
+        val ZERO = of(0.0, 0.0, 0.0)
+
+        fun of(x: Double, y: Double, z: Double): Vec3d =
+            AbsoluteVec3d(x, y, z)
+    }
+
+    val x: Double
+    val y: Double
+    val z: Double
+}
